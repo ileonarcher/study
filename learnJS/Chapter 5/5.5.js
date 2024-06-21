608a06369ee5c02dec8e555e9bf4037a4e3c3b4f
Nikita Mordvintsev <ileonarcher@gmail.com>
2024-06-21 23:25:11 +0300

    Update

--- learnJS/Chapter 5/5.5.js
+++ learnJS/Chapter 5/5.5.js
@@ -26,3 +26,30 @@ function copySorted(arr) {
   newArr.push(...arr);
   return newArr.sort((a, b) => a.localeCompare(b));
 }
+
+function Calculator() {
+  (this.calculate = function (str) {
+    let action = str.split(" ");
+    let a = Number(action[0]);
+    let b = Number(action[2]);
+    return this[action[1]](a, b);
+  }),
+    (this["+"] = (a, b) => a + b),
+    (this["-"] = (a, b) => a - b),
+    (this.addMethod = function (operator, func) {
+      this[operator] = func;
+    });
+}
+
+let names = users.reduce((acc, item) => {
+  acc.push(item.name);
+  return acc;
+}, []);
+
+let usersMapped = users.map((item) => {
+  return { fullName: `${item.name} ${item.surname}`, id: item.id };
+});
+
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age - b.age);
+}
